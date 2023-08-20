8ac526f44a4d002cf70ce39232ec3211d3d23de0
vosjedev <hetlachendevosje@gmail.com>
2023-08-20 13:08:05 +0200

    (untested) add isntall and update wizard

---
+++ ClosedAppRegister/updateapps.app
@@ -0,0 +1,4 @@
+Name=Update local apps
+Description=Update all local apps installed using the wizard
+ID=vosjedev_git_update_wizard
+Exec=python3 @__root_vosje__@/installwizard.py do-update
--- installwizard.py
+++ installwizard.py
@@ -1,3 +1,45 @@
 #!/usr/bin/python3
-print("currently a placeholder, ignore me")
-print("progress is coming later")
+import os,sys,shutil
+url=sys.argv[1]
+
+if url=="do-upgrade":
+    print("entering app dir")
+    os.chdir(os.path.dirname(__file__)+"/AppDir")
+    appnotupdatedcnt=0
+    for app in os.listdir():
+        if os.path.isdir(app):
+            os.chdir(app)
+            x=os.system("git pull")
+            if not x==0:
+                print(f"git failed with code {x}! Is it installed?")
+                appnotupdatedcnt=appnotupdatedcnt+1
+            os.chdir('..')
+    if appnotupdatedcnt>0:
+        print(f"{appnotupdatedcnt} apps not updated!")
+    exit()
+
+print("entering app dir")
+os.chdir(os.path.dirname(__file__)+"/AppDir")
+print("find name...")
+name=url.split('/')[-1]
+print("cloning...")
+x=os.system(f"git clone '{url}' '{name}'")
+if not x==0:
+    print(f"git failed with code {x}! Is it installed?")
+    exit(x)
+print("entering app dir...")
+os.chdir(name)
+print("finding app files...")
+if os.path.isdir("AppRegister"):
+    for file in os.listdir("AppRegister"):
+        if file.endswith(".app") and os.path.isfile(file):
+            shutil.copyfile(file,f"../../AppRegister/{file}")
+        elif file=="LivingDirectory":
+            print("Found file living directory, copying...")
+            f=open("LivingDirectory","r")
+            dest=f.read().split("\n")[0]
+            shutil.copytree(".",dest)
+            print("done")
+else:
+    print("not found, generating them instead")
+    print("currently not implemented... come back later, or implement yourself.")
